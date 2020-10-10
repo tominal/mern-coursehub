aa1d18adf4c2eb659cdb700dca92abaca0213fd6
Brice Allard <briceallard@gmail.com>
2020-10-10 14:16:20 -0500

    first commit - standard react app template

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+
+      {/* HEADER */}
+      {/* HOME */}
+      {/* FOOTER */}
+
+    </div>
+  );
+}
+
+export default App;
